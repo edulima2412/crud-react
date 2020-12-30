f4ee31f62ac89f76d26bfdb2ff6c35369e61a21c
Eduardo Lima <edulima2412@gmail.com>
2020-12-29 21:42:34 -0300

    initial commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Inicio" subtitle="Projeto simples CRUD">
+    <div className="display-4">Bem Vindo</div>
+    <hr />
+    <p className="mb-0">Sistema para exemplificar um cadastro de usuário</p>
+  </Main>
+);
